32b6cf030c277a1bda94012cbe22e86be5b1af24
Chase Simmons <csim6890@gmail.com>
2021-05-07 00:17:37 -0500

    cleanup navigate

--- src/scripts/navigate.js
+++ src/scripts/navigate.js
@@ -10,12 +10,6 @@ function navigate() {
 
   const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
 
-  // if (randomBool === 1) {
-  //   randomDirection = -(Math.random() * 1);
-  // } else {
-  //   randomDirection = Math.random() * 1;
-  // }
-
   return { x: Math.cos(angle), y: Math.sin(angle) };
 }
 
